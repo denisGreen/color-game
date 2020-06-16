a770021fd293010291d973ff9943264342bfe7ab
denisGreen <kolomiitsev.denis@gmail.com>
2020-06-16 12:45:34 +0300

    add colors to the app title in the Header

--- my-app/src/components/header/Header.jsx
+++ my-app/src/components/header/Header.jsx
@@ -4,7 +4,8 @@ import "./header.css";
 const Header = () => {
   return (
     <div className="header">
-      <h1 className="header-text centered">Color Cards Game</h1>
+      <h1 className="header-text centered"><span className="header-text-color-one">Color</span> <span
+      className="header-text-color-two">Cards</span> <span className="header-text-color-three">Game</span></h1>
       <h2 className="centered">Find two cards with the same color</h2>
     </div>
   );
